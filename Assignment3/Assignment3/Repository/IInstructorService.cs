c8d4461140564d061ee4e722a59eb1db163031bd
jackiejing <jackie.jingg1125@gmail.com>
2022-06-30 20:44:46 -0400

    Create IInstructorService.cs

---
+++ Assignment3/Assignment3/Repository/IInstructorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Repository
+{
+    public interface IInstructorService
+    {
+        public int BounsJoin(DateTime join);
+        public bool HeadDepartment(int workdays);
+    }
+}
