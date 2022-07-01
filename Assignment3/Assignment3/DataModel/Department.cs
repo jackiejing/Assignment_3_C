9d8393edc015604336fcdb2e168e8a66982234fe
jackiejing <jackie.jingg1125@gmail.com>
2022-06-30 20:43:41 -0400

    Create Department.cs

---
+++ Assignment3/Assignment3/DataModel/Department.cs
@@ -0,0 +1,25 @@
+﻿using Assignment3.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.DataModel
+{
+    public class Department : IDepartment
+    {
+        public string Headname { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public void PrintCourse()
+        {
+            string[] coursename = new string[10];
+            foreach (var item in coursename)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
