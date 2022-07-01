be4861b66b8c826c59b63cc78e9163e965683348
jackiejing <jackie.jingg1125@gmail.com>
2022-06-30 20:43:45 -0400

    Create Person.cs

---
+++ Assignment3/Assignment3/DataModel/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.DataModel
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public void situation()
+        {
+            Console.WriteLine("good");
+        }
+
+
+
+        public int CalAge(int birthyear)
+        {
+            int result;
+            int currentYear = DateTime.Now.Year;
+            result = currentYear - birthyear;
+            return result;
+        }
+
+        public decimal salary(int hour)
+        {
+            int result = (((hour * 7) * 4) * 12);
+            return result;
+        }
+
+        public string getaddress(string address)
+        {
+            Console.WriteLine("Enter your address");
+            string s = Console.ReadLine();
+
+            return s;
+        }
+    }
+}
