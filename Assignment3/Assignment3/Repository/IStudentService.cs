9d412499f560fdc3e02115b12a961144e08c1466
jackiejing <jackie.jingg1125@gmail.com>
2022-06-30 20:44:51 -0400

    Create IStudentService.cs

---
+++ Assignment3/Assignment3/Repository/IStudentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Repository
+{
+    public interface IStudentService
+    {
+        public string[] coursesName(int total);
+        public double Gpa();
+    }
+}
