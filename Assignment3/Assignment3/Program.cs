015bd437db8ee1b6c78f8d7d638348b459a04978
jackiejing <jackie.jingg1125@gmail.com>
2022-06-30 20:44:39 -0400

    Create Program.cs

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment3;
+//Console.WriteLine(Fibonacci.FibonacciValue(10));
+Ball ba = new Ball(12, 2, 32, 25, 10);
+ba.Throw();
+ba.Throw();
+ba.Throw();
+ba.Throw();
+ba.Throw();
+ba.Pop();
+ba.PrintCount();
