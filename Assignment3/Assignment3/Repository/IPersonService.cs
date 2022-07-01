ab8f513218463fcd597cc147adbb120caaa277f2
jackiejing <jackie.jingg1125@gmail.com>
2022-06-30 20:44:49 -0400

    Create IPersonService.cs

---
+++ Assignment3/Assignment3/Repository/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Repository
+{
+    public interface IPersonService
+    {
+        public int CalAge(int birthyear);
+        public decimal salary(int hour);
+        public string getaddress(string add);
+    }
+}
